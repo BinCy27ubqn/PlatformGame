ad0fe592b5712eaeff572600c4c2be5067cb9977
BinCy27ubqn <thaibinhdang2710@gmail.com>
2024-09-17 21:31:20 +0700

    Create enemy commit

---
+++ Assets/Scripts/Enemy/Marce.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marce : MonoBehaviour
+{
+    public float speed;
+    int direction = 1;
+    public float distance;
+    float startPoint;
+
+    private void Start()
+    {
+        startPoint = gameObject.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed*direction);
+        if(transform.position.y - startPoint > distance || transform.position.y == startPoint)
+        {
+            direction = direction * -1;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Saw.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    public float speed;
+    public Transform sawCheck;
+
+    bool isGroundCheck;
+    public Transform pointRay;
+    public float distanceRayCheck;
+
+    int direction = 1;
+    Vector2 rayDirection = new Vector3(1, -1);
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed);
+        
+        isGroundCheck = Physics2D.Raycast(pointRay.position, rayDirection, distanceRayCheck);
+        if (isGroundCheck)
+        {
+            sawCheck.Translate(Vector2.right * speed* direction);
+        }
+        if (!isGroundCheck)
+        {
+            direction = direction * -1;
+            rayDirection.x = rayDirection.x * -1;
+            sawCheck.Translate(Vector2.right * speed * direction);
+
+            Vector3 iTemp = sawCheck.localScale;
+            iTemp.x *= -1;
+            sawCheck.localScale = iTemp;
+        }
+
+        Debug.DrawRay(pointRay.position, rayDirection * distanceRayCheck, Color.red);
+
+    }
+}
